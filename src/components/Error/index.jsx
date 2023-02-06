b3961c6b0e7d1af3506740591dd7d0c1b26dd57d
Yann <ybarlet@gmail.com>
2023-02-06 22:18:16 +0100

    Router configuration

---
+++ src/components/Error/index.jsx
@@ -0,0 +1,10 @@
+function Error() {
+  return (
+    <div>
+      <h1>Oups 🙈 Cette page n'existe pas</h1>
+      <h2>404 Not Found</h2>
+    </div>
+  )
+}
+
+export default Error
---
+++ src/pages/Freelances/index.jsx
@@ -0,0 +1,9 @@
+function Freelances() {
+  return (
+    <div>
+      <h1>Freelances 👨🏽‍💻👨🏽‍💻👨🏽‍💻</h1>
+    </div>
+  )
+}
+
+export default Freelances
