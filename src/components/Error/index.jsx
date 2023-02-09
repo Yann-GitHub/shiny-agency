70a5cd001cc86d5baa0ca689ef09235e98e03bb3
Yann <ybarlet@gmail.com>
2023-02-09 15:19:48 +0100

    Styled Components configuration

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,9 +1,35 @@
+import styled from 'styled-components'
+import ErrorImg from '../../assets/404.svg'
+import colors from '../../utiles/style/color'
+
+const ErrorWrapper = styled.div`
+  margin: 30px;
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.background};
+  align-items: center;
+`
+
+const TopMessage = styled.h1`
+  font-weight: 300;
+`
+
+const BottomMessage = styled.h2`
+  font-weight: 300;
+  color: ${colors.secondary};
+`
+
+const ErrorImage = styled.img`
+  max-width: 800px;
+`
+
 function Error() {
   return (
-    <div>
-      <h1>Oups 🙈 Cette page n'existe pas</h1>
-      <h2>404 Not Found</h2>
-    </div>
+    <ErrorWrapper>
+      <TopMessage>Oups...</TopMessage>
+      <ErrorImage src={ErrorImg} alt="image" />
+      <BottomMessage>Il semblerait qu’il y ait un problème</BottomMessage>
+    </ErrorWrapper>
   )
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -7,10 +7,21 @@ import Header from './components/Header'
 import Error from './components/Error'
 import Freelances from './pages/Freelances'
 import Results from './pages/Results'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    * {
+      font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+    body {
+      margin: 0;
+    }
+`
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path="/">
